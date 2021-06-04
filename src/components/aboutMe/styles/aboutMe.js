20c556f80b12aece61df8512f687fc218f90a259
rotonda <rotondwamulelu@gmail.com>
2021-06-04 12:02:03 +0200

    fixed download butto

--- src/components/aboutMe/styles/aboutMe.js
+++ src/components/aboutMe/styles/aboutMe.js
@@ -61,7 +61,7 @@ export const Buttons = styled.div`
   margin: 0 10rem;
   margin-bottom: 5rem;
   @media (max-width: 320px) {
-    margin: 0 3rem;
+    margin: 0 3.4rem;
     margin-bottom: 5rem;
   }
 `;
