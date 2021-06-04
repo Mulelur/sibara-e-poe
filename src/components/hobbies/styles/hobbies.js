110354d51a45a02be4232cbff9156dda3301e121
rotonda <rotondwamulelu@gmail.com>
2021-06-04 11:26:02 +0200

    fixed hobbies res

--- src/components/hobbies/styles/hobbies.js
+++ src/components/hobbies/styles/hobbies.js
@@ -2,11 +2,20 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   margin: 10rem 15rem;
+  transition: all 0.5s;
+  @media (max-width: 800px) {
+    margin: 5rem 7.5rem;
+  }
+  @media (max-width: 725px) {
+    margin: 5rem 7.5rem;
+  }
+  @media (max-width: 500px) {
+    margin: 1.8rem 2.5rem;
+  }
 `;
 
 export const Content = styled.div`
   margin: 1.8rem;
-  width: 100%;
   padding: 2.3rem;
   transition: all 0.5s;
   box-shadow: 0 0 1px 0 rgba(6, 24, 44, 0.18), 0 1px 2px 0 rgba(6, 24, 44, 0.2);
